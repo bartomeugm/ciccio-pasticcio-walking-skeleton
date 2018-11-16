67535c583a875224b158c4d7d813cf53bcbe74a8
nawhi <25394965+nawhi@users.noreply.github.com>
2018-11-16 11:57:43 +0000

    Throw IllegalArgumentException from addEmployee()

---
+++ employees/src/main/java/ApplicationService.java
@@ -0,0 +1,5 @@
+public class ApplicationService {
+    public void addEmployee(Object o) {
+        throw new IllegalArgumentException();
+    }
+}
---
+++ employees/src/test/java/ApplicationServiceShould.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationServiceShould {
+
+    @Test
+    public void fail_if_no_data_provided_on_employee_creation() {
+
+        assertThrows(IllegalArgumentException.class, () -> {
+
+            new ApplicationService().addEmployee(null);
+        });
+
+    }
+}
