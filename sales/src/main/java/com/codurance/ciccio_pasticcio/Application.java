9198b5e0613c60ba4d2a4a15937192c9bcc15be5
Bartomeu <bartomeugm@gmail.com>
2018-11-16 17:06:33 +0100

    API Acceptance test with hardcoded values

--- sales/src/main/java/com/codurance/ciccio_pasticcio/Application.java
+++ sales/src/main/java/com/codurance/ciccio_pasticcio/Application.java
@@ -1,17 +1,21 @@
 package com.codurance.ciccio_pasticcio;
 
+import com.eclipsesource.json.Json;
 import com.google.gson.Gson;
 import spark.Spark;
 
-import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class Application {
     public static void main(String... args) {
         OrderController orderController = ControllerFactory.orderController();
         setPort();
-        get("/order", (req, res) ->
-                        orderController.sayHello(req, res)
-                , Application::toJson);
+        post("/orders", (req, res) -> {
+                    res.status(201);
+                    res.type("application/json");
+                    return Json.object().add("url", "/orders/1");
+                }
+        );
     }
 
     private static void setPort() {
